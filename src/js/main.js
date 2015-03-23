215a0fc7bc7202cf919b5da25daec8471ba4e32e
Brandon Jones <brandon.jones1985@gmail.com>
2015-03-23 19:18:48 -0400

    get the JQ working correctly, add basic CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,20 @@
-$.getJSON("webdevtest-data.js", function(person){
-	alert("JSON Data : " + json.name);
+$( document ).ready(function() {
+	
+	//$.getJSON('http://dev.samluedke.com/webdevtest-master/src/js/webdevtest-data.js', function(data){
+	  //  console.log("This is the Data" + data["server_time"])
+	//});
+	
+	$.getJSON( "http://dev.samluedke.com/webdevtest-master/src/js/webdevtest-data.js", function(data) {
+	  var items = [];
+	  $.each( data, function( key, val ) {
+	    items.push( "<li id='" + key + "'>" + val + "</li>" );
+	  });
+
+	  $( "<ul/>", {
+	    "class": "my-new-list",
+	    html: items.join( "" )
+	  }).appendTo( "div.content" );
+	});
+	
+
 });
