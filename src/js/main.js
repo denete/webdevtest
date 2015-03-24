a562962760210d0935ca4e78de279fd2aa66b530
Brandon Jones <brandon.jones1985@gmail.com>
2015-03-24 14:55:46 -0400

    main JS cleanup, responsive CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {   
  
 	
-	    
+	// List    
 	$.getJSON( "http://dev.samluedke.com/webdevtest-master/src/js/webdevtest-data.js", function(data) { 
 	  	var items = [];
 		  $.each( data.promotion_objects, function( key, val ) {
@@ -10,16 +10,8 @@ $( document ).ready(function() {
 			items.push( "<div id='summary'>" + val.summary + "</div>" );
 			items.push( "<div id='entry_info'>" + val.entry_info + "</div>" );	
 				$.each(val.drawings, function(key2,val2) {
-						// items.push("<div class='group-drawings'><div id='prize'>"+val2.prize+"</div>");
-						// items.push("<div id='entry_deadline'>"+val2.entry_deadline+"</div>"); 
 						items.push("<div id='drawing_date'>"+val2.drawing_date+"</div></div>");					
-			    });
-				
-				// $.each(val.entries, function(key3,val3) {
-				// 			            items.push("<div class='group-entries'><div id='entry_number'>"+val3.entry_number+"</div>");
-				// 						items.push("<div id='date'>"+val3.date+"</div></div>");
-				// 			     });  
-			
+			    });			
 		  });
 
 		  $( "<div/>", {
