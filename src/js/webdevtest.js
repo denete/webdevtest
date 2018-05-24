3f4136dd48acee88bf09fba4e1a82f9bfdb7dd15
SevN <peter.paskovits@gmail.com>
2018-05-24 17:17:23 +0200

    iterating list items, getting next drawing date

--- src/js/webdevtest.js
+++ src/js/webdevtest.js
@@ -1,3 +1,16 @@
+window.onload = () => {
+    getScript('https://code.jquery.com/jquery-1.10.2.js', () => {
+
+        dataHandler = Object.assign({}, dataLoader(dataProviderXML(), dataParserJSON()));
+
+        dataHandler.loadData('js/webdevtest-data.js', (data) => {
+            const promoId = getURLParameter("promo");
+            let view = promoId ? new PromotionView(data, promoId) : new ListView(data);
+            view.display();
+        })
+    });
+}
+
 function getScript (src, callback) {
     var headElem = document.head || document.getElementsByTagName('head')[0];
     var script = document.createElement("script");
@@ -16,15 +29,13 @@ function getScript (src, callback) {
     headElem.appendChild(script);
 }
 
-function getURLParameter(sParam) {
-    var sPageURL = window.location.search.substring(1);
-    var sURLVariables = sPageURL.split('&');
-    for (var i = 0; i < sURLVariables.length; i++)
-    {
-        var sParameterName = sURLVariables[i].split('=');
-        if (sParameterName[0] == sParam)
-        {
-            return sParameterName[1];
+function getURLParameter(paramaterName) {
+    var pageURL = window.location.search.substring(1);
+    var parameters = pageURL.split('&');
+    for (var i = 0; i < parameters.length; ++i) {
+        var  parameter = parameters[i].split('=');
+        if (parameter[0] == paramaterName) {
+            return parameter[1];
         }
     }
     return "";
@@ -77,11 +88,37 @@ const dataLoader = (dataProvider, dataParser) => ({
 
 class AbstractView {
     constructor(data) {
-        this.data;
+        this.data = data;
+        this.server_time = new Date(data.server_time);
+        this.dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     }
 
     display() {
         console.error("Abstract View is abstract!");
+    }
+
+    getValidDrawings(promotion) {
+        return promotion.drawings.filter(drawing => {
+            let date = new Date(drawing.drawing_date);
+            return date && date > this.server_time;
+        });
+    }
+
+    getNextDrawing(promotion) {
+        let validDrawingDates = this.getValidDrawings(promotion);
+        if (validDrawingDates && validDrawingDates.length) {
+            return validDrawingDates.reduce((min, date) => date < min ? date : min, validDrawingDates[0]);
+        }
+        return null;
+    }
+
+    getNextDrawingDateString(promotion) {
+        let nextDrawing = this.getNextDrawing(promotion);
+        if (nextDrawing) {
+            let date = new Date(nextDrawing.drawing_date);
+            return date.toLocaleDateString("en-US", this.dateOptions);
+        }
+        return "";
     }
 }
 
@@ -91,7 +128,29 @@ class ListView extends AbstractView {
     }
 
     display() {
-        console.log("List View");
+        this.data.promotion_objects.forEach(promotion => {
+            let item =  $('<div>');
+
+            let itemImg = $('<img>');
+            itemImg.attr('src', promotion.promo_image_url);
+            itemImg.appendTo(item);
+
+            let itemName = $('<div>');
+            itemName.text(promotion.promotion_name);
+            itemName.appendTo(item);
+
+            let itemSummary = $('<div>');
+            itemSummary.text(promotion.summary);
+            itemSummary.appendTo(item);
+            
+            let itemDrawing = $('<div>');
+
+            itemDrawing.text('Next Drawing Date: '+this.getNextDrawingDateString(promotion));
+            itemDrawing.appendTo(item);
+
+            item.appendTo('.promotion-list');
+        });
+        $('.promotion-list').show();
     }
 }
 
@@ -106,21 +165,4 @@ class PromotionView extends AbstractView {
     }
 }
 
-window.onload = () => {
-    getScript('https://code.jquery.com/jquery-1.10.2.js', () => {
-
-        dataHandler = Object.assign({}, dataLoader(dataProviderXML(), dataParserJSON()));
-
-        dataHandler.loadData('js/webdevtest-data.js', (data) => {
-            const promoId = getURLParameter("promo");
-            let view;
-            if (promoId) {
-                view = new PromotionView(data, promoId);
-            } else {
-                view = new ListView(data);
-            }
-            view.display();
-        })
-    });
-}
 
