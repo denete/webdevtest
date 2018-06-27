92cbdd375a80eec50eb8050125955ea05762bceb
Mihaly Tari <tari.mihaly@gmail.com>
2018-06-27 12:18:25 +0200

    Setup react app

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import reduxThunk from "redux-thunk";
+// import axios from "axios";
+
+// import App from "./components/App";
+// import reducers from "./reducers";
+
+// window.axios = axios;
+
+// const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+// const jsx = (
+// 	<Provider store={store}>
+// 		<App />
+// 	</Provider>
+// );
+
+const jsx = (
+    <div>TEST</div>
+)
+
+ReactDOM.render(jsx, document.getElementById("root"));
